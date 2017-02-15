dbd38b7f22d9097cedc2e87681a822960406622b
eason <zhaoyi@asiainfo.com>
2017-02-15 10:42:20 +0800

    @init

---
+++ lazyBoneRoot/lazybone/src/main/java/com/ai/LazyboneApplication.java
@@ -0,0 +1,12 @@
+package com.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyboneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyboneApplication.class, args);
+	}
+}
