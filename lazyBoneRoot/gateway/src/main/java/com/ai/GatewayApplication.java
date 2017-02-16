875bab3ea18692ce0b6cd045f407ca92901b61ab
eason <zhaoyi@asiainfo.com>
2017-02-16 10:36:40 +0800

    @sync

--- lazyBoneRoot/gateway/src/main/java/com/ai/GatewayApplication.java
+++ lazyBoneRoot/gateway/src/main/java/com/ai/GatewayApplication.java
@@ -2,8 +2,12 @@ package com.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 
 @SpringBootApplication
+@EnableSidecar
+@EnableHystrixDashboard
 public class GatewayApplication {
 
 	public static void main(String[] args) {
