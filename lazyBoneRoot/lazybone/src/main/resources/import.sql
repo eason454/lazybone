24fe74a5871c7efddd114d728111f8daf7ab361c
jerry <303301416@qq.com>
2017-02-16 17:28:47 +0800

    modified:   lazybone/src/main/resources/import.sql

--- lazyBoneRoot/lazybone/src/main/resources/import.sql
+++ lazyBoneRoot/lazybone/src/main/resources/import.sql
@@ -1,13 +1,18 @@
 insert into lazy_bone_course(id,description,create_date,name,update_date,url,course_days)
-  values(10,'塑臀',now(),'塑臀',now(),'http://ac-8ueciueo.clouddn.com/068Qob0G6Y6ABlcmXdVMjCPJxVq0pZcOqfUebKVd.png',30),
+  values(10,'塑臀',now(),'塑臀',now(),'http://ac-8ueciueo.clouddn.com/068Qob0G6Y6ABlcmXdVMjCPJxVq0pZcOqfUebKVd.png',30);
+insert into lazy_bone_course(id,description,create_date,name,update_date,url,course_days)
         (11,'快速的生活节奏、不小的生活压力char(10) 鞭打着奋斗一族前进的同时char(10) 也导致了让人苦恼的各种亚健康问题char(10) 僵硬的肩颈、腰背char(10) 久坐囤积的"游泳圈"char(10) 紧张不得放松的神经char(10) 胀气的肠道、抗议的胃char(10) 如果你有以上症状',
-         now(),'办公室恢复',now(),'http://ac-8ueciueo.clouddn.com/daKvQelA0VhT4qv4JfkfPnSd8ARMsp1WsJxIToeL.png',30),
+         now(),'办公室恢复',now(),'http://ac-8ueciueo.clouddn.com/daKvQelA0VhT4qv4JfkfPnSd8ARMsp1WsJxIToeL.png',30);
+insert into lazy_bone_course(id,description,create_date,name,update_date,url,course_days)
          (12,'跳结石',now(),'跳结石',now(),'http://ac-8ueciueo.clouddn.com/s9DKy54iBq6Ra4c2gwyeoA98xtK6Bo39MlhBY04d.png',7);
 
 insert into fit_action(id,action_description,action_type,action_name,count_unit,create_date,state)
-  values (1,'下蹲',0,'下蹲','次',now(),1),
-         (2,'跳跃',1,'跳跃','次',now(),1),
-         (3,'交叉跳',3,'交叉跳','次',now(),1),
+  values (1,'下蹲',0,'下蹲','次',now(),1);
+insert into fit_action(id,action_description,action_type,action_name,count_unit,create_date,state)
+         (2,'跳跃',1,'跳跃','次',now(),1);
+insert into fit_action(id,action_description,action_type,action_name,count_unit,create_date,state)
+         (3,'交叉跳',3,'交叉跳','次',now(),1);
+insert into fit_action(id,action_description,action_type,action_name,count_unit,create_date,state)
          (4,'俯卧撑',2,'俯卧撑','次',now(),1);
 
 insert into course_item(course_item_id, create_date, exercise_unit, image_url, item_name, require_times, update_date, course_id, exercise_type)
