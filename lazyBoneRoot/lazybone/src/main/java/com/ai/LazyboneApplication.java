2567cc39ade825c7bf07bdf104fd4a19b0209638
eason <zszml454@gmail.com>
2017-02-21 15:25:11 +0800

    提交异常处理代码

--- lazyBoneRoot/lazybone/src/main/java/com/ai/LazyboneApplication.java
+++ lazyBoneRoot/lazybone/src/main/java/com/ai/LazyboneApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LazyboneApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("spring.profiles.active","dev");
 		SpringApplication.run(LazyboneApplication.class, args);
 	}
 }
