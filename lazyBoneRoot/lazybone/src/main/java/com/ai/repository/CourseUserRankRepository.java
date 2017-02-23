0b2a1b74d2567a96a74a2bc11d5ea070470db90b
zhangyi <zhangyi@asiainfo.com>
2017-02-23 13:01:39 +0800

    fix rank error

--- lazyBoneRoot/lazybone/src/main/java/com/ai/repository/CourseUserRankRepository.java
+++ lazyBoneRoot/lazybone/src/main/java/com/ai/repository/CourseUserRankRepository.java
@@ -2,6 +2,8 @@ package com.ai.repository;
 
 import java.util.Date;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +13,7 @@ import com.ai.domain.CourseUserRank;
 
 public interface CourseUserRankRepository extends JpaRepository<CourseUserRank, String> {
 
+	@Transactional
 	void deleteByRankDate(Date date);
 
 	Page<CourseUserRank> findByCourseIdAndRankDateBetweenOrderByRank(String courseId, Date startDate, Date endDate,
